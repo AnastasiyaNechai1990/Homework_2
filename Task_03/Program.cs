805168d9d65488784f824a2781bd66c1aa832ee6
Anastasiya <anastasiya.sachuk@mail.ru>
2023-01-18 22:53:45 +0300

    Add gitignore

---
+++ Task_03/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+/*6 -> да
+7 -> да
+1 -> нет*/
+
+Console.Clear();
+
+Console.WriteLine("Введите порядковый номер дня недели: ");
+int num = int.Parse(Console.ReadLine()!);
+int numbers = [1, 2, 3, 4, 5];
+int count = 7;
+
+while (num <= count)
+{
+  if (num == numbers)
+  {
+    Console.WriteLine($"{num} -> нет");
+  }
+
+  else
+  {
+    Console.WriteLine($"{num} -> да");
+  }   
+}
+Console.WriteLine("Дня недели с данным порядковым номером не существует!!!");
+
+ 
+
+
