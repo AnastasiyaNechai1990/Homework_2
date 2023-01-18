9a98edadddca8c5c04f551e8044e28b38120ef0c
Anastasiya <anastasiya.sachuk@mail.ru>
2023-01-18 21:05:06 +0300

    Add Task 01

---
+++ Task_01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+/*456 -> 5
+782 -> 8
+918 -> 1*/
+
+Console.Clear();
+
+Console.WriteLine("Введите трёх значное число: ");
+int num = int.Parse(Console.ReadLine()!);
+int numA = num % 100;
+int result = numA / 10;
+Console.WriteLine($"{num} -> {result}");
